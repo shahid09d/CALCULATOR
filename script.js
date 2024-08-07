8c87b6c46c365c2dbe4761b4ee26c1afa9a6a1b1
Shahid Maqbool <141574607+shahid09d@users.noreply.github.com>
2024-08-07 18:08:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,16 @@ arr.forEach(button => {
         if (e.target.innerHTML === '=') {
             string = eval(string);
             input.value = string;
-        } else if (e.target.innerHTML === 'AC') {
+        } 
+        else if (e.target.innerHTML === 'AC') {
             string = "";
             input.value = string;
-        } else {
+        } 
+        else if(e.target.innerHTML === 'DLT'){
+          string = string.substring(0, string.length-1);
+          input.value = string;
+        }
+        else {
             string += e.target.innerHTML;
             input.value = string;
         }
